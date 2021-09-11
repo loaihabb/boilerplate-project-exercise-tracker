d367cc53ee7c3f3655d2ba9e214f23b7b1a1beec
John Albright <John@john-albrights-macbook-pro.local>
2021-09-10 23:30:09 -0500

    Added models and installed mongoose

---
+++ models/exercise.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const exerciseSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+});
+
+mongoose.exports = mongoose.model('Exercise', exerciseSchema);

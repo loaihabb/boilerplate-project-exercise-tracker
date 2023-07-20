c740e3eea5ca01bd12d345df093e60cdb9fa36fb
LoiyHabeeb <loaihabb@gmail.com>
2023-07-20 19:22:43 +0300

    exercise-tracker

--- server.js
+++ server.js
@@ -307,3 +307,4 @@ app.get('/api/exercises', (req, res) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 })
+
